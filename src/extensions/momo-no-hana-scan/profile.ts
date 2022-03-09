3d8b9c6022b5c5b87e58a55d5c1b55cef719b4dd
SDMasterGames <sdmastergames@hotmail.com>
2022-03-09 03:07:39 -0400

    profile

--- src/extensions/momo-no-hana-scan/profile.ts
+++ src/extensions/momo-no-hana-scan/profile.ts
@@ -3,7 +3,7 @@ import { ProfileScan } from "../../entidades/ProfileScan";
 const profile = new ProfileScan({
     name:"Momo No Hana Scan",
     logo_uri:"https://cdn.statically.io/img/momonohanascan.com/wp-content/uploads/2017/10/LOGO.png?quality=90&f=auto",
-    website_uri:"https://www.momonohanascan.com/",
+    website_uri:"https://www.momonohanascan.com",
     recruiting_uri:"https://www.momonohanascan.com/recrutamento/",
     donation_uri:"https://www.momonohanascan.com/doacao/",
     social:[
