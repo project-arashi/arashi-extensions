4305219468b94cfbd2703d3377884503639670ec
SDMasterGames <sdmastergames@hotmail.com>
2022-03-09 19:22:45 -0400

    getProjectByRelease completo

--- src/extensions/momo-no-hana-scan/projects.ts
+++ src/extensions/momo-no-hana-scan/projects.ts
@@ -43,7 +43,7 @@ export class MomoNoHanaScansProject implements IProjectsController {
     $(".item__wrap").each((i, element) => {
       const chapterItem = $(element).find(".chapter > a").first();
       const slider__thumb = $(element).find(".slider__thumb");
-        const id = slider__thumb.find("a").attr("href") || "";
+      const id = slider__thumb.find("a").attr("href") || "";
       highlights.push({
         title:
           $(element)
@@ -53,8 +53,7 @@ export class MomoNoHanaScansProject implements IProjectsController {
             .replace(/\n/gi, "") || "",
         link: chapterItem.attr("href") || "",
         lastChapter: chapterItem.text() || "",
-        id:
-          this.getSlugProjectByUri(id) || "",
+        id: this.getSlugProjectByUri(id) || "",
         cover_uri: slider__thumb.find("img").attr("src") || "",
       });
     });
@@ -79,7 +78,39 @@ export class MomoNoHanaScansProject implements IProjectsController {
     return { lastestUpdates, highlights };
   }
 
-  async getProjectByRelease(project: ReleaseProject): Promise<void | Project> {}
+  async getProjectByRelease(project: ReleaseProject): Promise<void | Project> {
+    const { data, status } = await this.router("/manga/" + project.id);
+
+    if (status !== 200) {
+      throw new Error("Falha ao carregar a pagina do projeto");
+    }
+
+    const $ = cheerio.load(data);
+    const infos = new Map<string, string>();
+    const description = $(".summary__content.show-more").text().trim();
+    const painelInfos = $(".summary_content");
+    const statusProject = painelInfos
+      .find(".summary-content")
+      .last()
+      .text()
+      .trim();
+    $(".post-content_item").each((i, e) => {
+      const key = $(e).find(".summary-heading").text().trim();
+      const value = $(e).find(".summary-content").text().trim();
+      infos.set(key, value);
+    });
+    const genres = infos.get("Gênero(s)")?.split(", ") || [];
+    return {
+      ...project,
+      description,
+      status: statusProject,
+      adult: genres.includes("Adulto") || genres.includes("Horror"),
+      alt_title: infos.get("Nome Alternativo") || "",
+      artist: infos.get("Artista(s)") || "Desconhecido",
+      author: infos.get("Autor(es)") || "Desconhecido",
+      genres,
+    };
+  }
   async getChaptersByProject(
     item: ReleaseProject | Project
   ): Promise<void | Project> {}
@@ -89,4 +120,15 @@ export class MomoNoHanaScansProject implements IProjectsController {
   async getProjectsBySearch(search: string): Promise<void | ReleaseProject[]> {}
 }
 
-new MomoNoHanaScansProject("https://www.momonohanascan.com/").getHome();
+const ReleaseProjectTest = new ReleaseProject({
+  id: "black-kanojo",
+  title: "Black Kanojo",
+  cover_uri:
+    "https://cdn.statically.io/img/www.momonohanascan.com/wp-content/uploads/2021/10/9784040682402-193x278.jpg?quality=90&f=auto",
+  link: "https://www.momonohanascan.com/manga/black-kanojo/",
+  lastChapter: "Capítulo 20 - FIM ",
+});
+
+new MomoNoHanaScansProject(
+  "https://www.momonohanascan.com/"
+).getProjectByRelease(ReleaseProjectTest);
