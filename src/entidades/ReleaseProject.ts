46bcb5db00d46338529353ddada8421814565f58
SDMasterGames <sdmastergames@hotmail.com>
2022-03-09 01:49:40 -0400

    removendo a criação de teste

--- src/entidades/ReleaseProject.ts
+++ src/entidades/ReleaseProject.ts
@@ -8,14 +8,4 @@ export class ReleaseProject {
   constructor(props: ReleaseProject) {
     Object.assign(this, props);
   }
-  static createTest(): ReleaseProject {
-    return {
-      id: "takopii-no-genzai",
-      title: "Takopii no Genzai",
-      cover_uri:
-        "http://amascan.com/uploads/manga/takopii-no-genzai/cover/cover_250x350.jpg",
-      lastChapter: "#2. A Aventura de Takopii",
-      link: "http://amascan.com/manga/takopii-no-genzai/2",
-    };
-  }
 }
