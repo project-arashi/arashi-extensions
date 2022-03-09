5b0096f4da029b05b407bfb778c22492d60ee0da
SDMasterGames <sdmastergames@hotmail.com>
2022-03-09 02:43:03 -0400

    repositorio para os projects manter um padrão de criação

---
+++ src/repositorios/IProjectsController.ts
@@ -0,0 +1,15 @@
+import { CheerioAPI } from "cheerio";
+import { Chapter } from "../entidades/Chapter";
+import { Project } from "../entidades/Project";
+import { ReleaseProject } from "../entidades/ReleaseProject";
+
+export interface IProjectsController {
+  getHome(): Promise<{
+    lastestUpdates: ReleaseProject[];
+    highlights: ReleaseProject[];
+  } | void>;
+  getChaptersByProject(item: ReleaseProject | Project): Promise<Project | void>;
+  getPagsByChapter(chapter: Chapter): Promise<Chapter | void>;
+  getProjectsBySearch(search: string): Promise<ReleaseProject[] | void>;
+  getProjectsByGenre(genres: string): Promise<ReleaseProject[] | void>;
+}
